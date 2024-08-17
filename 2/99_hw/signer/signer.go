0115b52e44de8c8660de98f294d48b0584690a8e
S1aks <vh78aks@google.com>
2024-08-17 16:16:21 +0300

    Update crc32 function in signer.go

--- 2/99_hw/signer/signer.go
+++ 2/99_hw/signer/signer.go
@@ -1,10 +1,14 @@
 package main
 import (
-   "fmt"
+  "fmt"
 )
 
 func SingleHash(in, out chan interface{}) {
-  
+  data := <-in
+  crc32 := DataSignerCrc32(data.(string))
+  crc32md5 := DataSignerCrc32(DataSignerMd5(data.(string)))
+  result := crc32 + "~" + crc32md5
+  out <- result
 }
 
 func MultiHash(in, out chan interface{}) {
